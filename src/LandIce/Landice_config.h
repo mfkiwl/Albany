a31295622b1b4447aa906e82f5ee9aeb90f78017
Luca Bertagna <lbertag@sandia.gov>
2022-12-19 16:14:51 -0700

    Modularize config.h files, to minimize code rebuilds

---
+++ src/LandIce/Landice_config.h
@@ -0,0 +1,11 @@
+#ifndef LANDICE_CONFIG_H
+#define LANDICE_CONFIG_H
+
+// --- LandIce options
+#cmakedefine CISM_HAS_LANDICE
+#cmakedefine MPAS_HAS_LANDICE
+#cmakedefine CISM_CHECK_COMPARISONS
+#cmakedefine REDUCED_COMM
+#cmakedefine CISM_USE_EPETRA
+
+#endif // LANDICE_CONFIG_H
